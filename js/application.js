b8c77fb03d628047dfb8f520d9a5ded44a6293b0
unknown <Student@458S-G76T9Y1.students.dc_schools.k12.dc.us>
2015-01-05 11:53:47 -0500

    HBS

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
 window.SilverTick = Ember.Application.create();
-
+SilverTick.ApplicationSerializer = DS.LSSerializer.extend();
 SilverTick.ApplicationAdapter = DS.LSAdapter.extend({
   namespace: 'silvertick-emberjs'
 });
